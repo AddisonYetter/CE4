1e4e9a0f3eb7e5314e47c994beba10b0129cb044
ajy10 <ajy10@FSTU603013.pti.edu>
2018-10-24 15:45:41 -0400

    Added a hello line.

---
+++ src/ce4yetter/CE4Yetter.java
@@ -0,0 +1,12 @@
+package ce4yetter;
+public class CE4Yetter 
+{
+    public static void main(String[] args) 
+    {
+        hi();
+    }
+    public static void hi()
+    {
+        System.out.println("Github is pretty neat ~Addison");
+    }
+}
